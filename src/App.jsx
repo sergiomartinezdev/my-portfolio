96ff69d34d1da29465848865497c0730814f9e22
Sergio Martinez <sergiomartinez@Sergios-MacBook-Air.local>
2025-08-15 17:46:02 -0500

    Initial portfolio setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-import Home from "./components/Home"
-import Skills from "./components/Skills"
-import Contact from "./components/Contact"
+import React from "react";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="landing">
-        <div>
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React + Sergio</h1>
-        <div className="card">
-          <button onClick={() => setCount(count => count + 1)}>
-            count is {count}
-          </button>
-          <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-
+    <div>
       <Home />
       <Skills />
       <Contact />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
-
+export default App;
