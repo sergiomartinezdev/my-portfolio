2cc087ffd19e4900b43c558c04196db2a3ba3d3b
Sergio Martinez <sergiomartinez@Sergios-MacBook-Air.local>
2025-08-15 19:05:53 -0500

    Initial portfolio setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import './App.css';
 
 function App() {
   return (
